9372ea4b627a1ebf2d5642aac53cdc47ecef79ee
jiaxinhuang <924785452@qq.com>
2017-04-28 15:24:33 +0800

    update 0428

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.models import Page, Category
+from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
@@ -21,3 +21,11 @@ class PageForm(forms.ModelForm):
 	class Meta:
 		model = Page
 		exclude = ('category',)
+
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+		if url and not url.startswith('http://'):
+			url = 'http://' + url
+			cleaned_data['url'] = url
+		return cleaned_data
