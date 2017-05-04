9b7f1adc6a941fb635be503b203913d4ed191fc4
jiaxinhuang <924785452@qq.com>
2017-05-04 17:21:28 +0800

    add the part of template

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,9 +1,20 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 	views = models.IntegerField(default=0)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,8 +3,38 @@ from django.http import HttpResponse
 
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
+from rango.forms import UserForm, UserProfileForm
 
 # Create your views here.
+def register(request):
+	registered = False
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+			user.set_password(user.password)
+			user.save()
+			profile = profile_form.save(commit=False)
+			profile.user = user
+
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['picture']
+
+			profile.save()
+			registered = True
+		else:
+			print user_form.errors, profile_form.errors
+	else:
+		user_form = UserForm()
+		profile_form = UserProfileForm()
+
+	return render(request, 'rango/register.html',
+		          {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+
+
 def add_page(request, category_name_slug):
 	try:
 		category = Category.objects.get(slug=category_name_slug)
@@ -35,7 +65,8 @@ def add_category(request):
 		form = CategoryForm(request.POST)
 
 		if form.is_valid():
-			form.save(commit=True)
+			category = form.save(commit=True)
+			print category, category.slug
 			return index(request)
 		else:
 			print form.errors
